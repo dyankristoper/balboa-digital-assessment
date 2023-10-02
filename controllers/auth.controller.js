9cfe5f025ece691886a959a5d238e2f7e85c9df9
dyankristoper <jc@estelstudio.com>
2023-10-03 02:45:16 +0800

    Feat: Implement sign up and login with bcrypt and jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,29 @@
+require('dotenv').config();
+
+const { sequelize } = require('../config/server');
 const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const authenticate = ( request, response ) => {
     sequelize.sync().then(() => {
 
         User.findOne({
-            password: request.body.password
+            username: request.body.username
         })
-        .then(res => {
-            
+        .then(async (res) => {
+
             // This means that username exists
+            const comparison = await bcrypt.compare( res.password, request.body.password );
+
+            if( comparison ){
+                response
+                .status(200)
+                .send({ token: jwt.sign( res.username, process.env.SECRET ) });
+            }else{
+                // Unauthenticated
+                response.status( 401 ).send();
+            }
 
         }).catch((error) => {
             console.error('Failed to retrieve data : ', error);
@@ -20,4 +34,31 @@ const authenticate = ( request, response ) => {
     });
 }
 
-module.exports = { authenticate }
+const createUser = ( request, response ) => {
+    sequelize.sync().then( async () => {
+
+        const { username, password } = request.body;
+
+        console.log( username );
+        console.log( password );
+
+        // 10 as SALT rounds
+        const hash = await bcrypt.hash( password, 10 );
+
+        User.create({
+            username: username,
+            password: hash
+        }).then(res => {
+            response
+            .status( 201 )
+            .send({ status: "User created successfully." });
+        }).catch((error) => {
+            console.error('Failed to create a new record : ', error);
+        });
+    
+    }).catch((error) => {
+        console.error('Unable to create table : ', error);
+    });
+}
+
+module.exports = { authenticate, createUser }
